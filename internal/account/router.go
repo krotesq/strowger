49714715e17b9b3edf5d4bfeb958914b3ce91260
aaronlyy <aaronlevican@gmail.com>
2026-04-21 14:04:16 +0200

    feat: added all needed routers

--- internal/account/router.go
+++ internal/account/router.go
@@ -1,1 +1,11 @@
 package account
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func RoutesWithPool(pool *pgxpool.Pool) chi.Router {
+	router := chi.NewRouter()
+	return router
+}
