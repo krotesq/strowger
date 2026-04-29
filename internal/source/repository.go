a4a1d5d54c4c712a68fd76947717df45987cc1a5
hegi64 <gamerhegi64@gmail.com>
2026-04-29 23:18:29 +0200

    add findWithRtmp, createRtmp and delete to source and target Repositories

--- internal/source/repository.go
+++ internal/source/repository.go
@@ -1,6 +1,10 @@
 package source
 
 import (
+	"context"
+	"database/sql"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -12,4 +16,79 @@ type repository struct {
 
 func newRepository(pool *pgxpool.Pool) *repository {
 	return &repository{pool: pool}
+}
+
+func (repository *repository) findWithRtmpByID(ctx context.Context, id string) (*source, *rtmp, error) {
+	var _source source
+	const sourceQuery = `SELECT uuid, name, description, source_type_id, account_id, created_at FROM source WHERE uuid = $1`
+	if err := pgxscan.Get(ctx, repository.pool, &_source, sourceQuery, id); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	var _rtmp rtmp
+	const rtmpQuery = `SELECT source_id, url, stream_key, created_at FROM source_rtmp WHERE source_id = $1`
+	if err := pgxscan.Get(ctx, repository.pool, &_rtmp, rtmpQuery, _source.ID); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	return &_source, &_rtmp, nil
+}
+
+func (repository *repository) createWithRtmp(ctx context.Context, name string, description string, accountID string, url string, stream_key string) (*source, *rtmp, error) {
+	tx, err := repository.pool.Begin(ctx)
+	if err != nil {
+		return &source{}, &rtmp{}, err
+	}
+	defer tx.Rollback(ctx)
+
+	var sourceType _type
+	const sourceTypeQuery = `SELECT id, name FROM source_type WHERE name = 'rtmp'`
+	if err := pgxscan.Get(ctx, tx, &sourceType, sourceTypeQuery); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	const sourceQuery = `
+			INSERT INTO source (name, description, source_type_id, account_id)
+			VALUES (
+				$1,
+				$2,
+				$3,
+				$4
+			)
+			RETURNING uuid, name, description, source_type_id, account_id, created_at
+		`
+	var _source source
+	if err := pgxscan.Get(ctx, tx, &_source, sourceQuery, name, description, sourceType.ID, accountID); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	const rtmpQuery = `
+			INSERT INTO source_rtmp (source_id, url, stream_key)
+			VALUES ($1, $2, $3)
+			RETURNING source_id, url, stream_key, created_at
+		`
+	var _rtmp rtmp
+	if err := pgxscan.Get(ctx, tx, &_rtmp, rtmpQuery, _source.ID, url, stream_key); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return &source{}, &rtmp{}, err
+	}
+
+	return &_source, &_rtmp, err
+}
+
+func (repository *repository) delete(ctx context.Context, id string) error {
+	const query = `
+		DELETE FROM source
+		WHERE uuid = $1
+	`
+	cmdTag, err := repository.pool.Exec(ctx, query, id)
+
+	if cmdTag.RowsAffected() == 0 {
+		return sql.ErrNoRows
+	}
+
+	return err
 }
--- internal/target/repository.go
+++ internal/target/repository.go
@@ -1,6 +1,10 @@
 package target
 
 import (
+	"context"
+	"database/sql"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -12,4 +16,79 @@ type repository struct {
 
 func newRepository(pool *pgxpool.Pool) *repository {
 	return &repository{pool: pool}
+}
+
+func (repository *repository) findWithRtmpByID(ctx context.Context, id string) (*target, *rtmp, error) {
+	var _target target
+	const targetQuery = `SELECT uuid, name, description, target_type_id, account_id, created_at FROM target WHERE uuid = $1`
+	if err := pgxscan.Get(ctx, repository.pool, &_target, targetQuery, id); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	var _rtmp rtmp
+	const rtmpQuery = `SELECT target_id, url, stream_key, created_at FROM target_rtmp WHERE target_id = $1`
+	if err := pgxscan.Get(ctx, repository.pool, &_rtmp, rtmpQuery, _target.ID); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	return &_target, &_rtmp, nil
+}
+
+func (repository *repository) createWithRtmp(ctx context.Context, name string, description string, accountID string, url string, stream_key string) (*target, *rtmp, error) {
+	tx, err := repository.pool.Begin(ctx)
+	if err != nil {
+		return &target{}, &rtmp{}, err
+	}
+	defer tx.Rollback(ctx)
+
+	var targetType _type
+	const targetTypeQuery = `SELECT id, name FROM target_type WHERE name = 'rtmp'`
+	if err := pgxscan.Get(ctx, tx, &targetType, targetTypeQuery); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	const targetQuery = `
+			INSERT INTO target (name, description, target_type_id, account_id)
+			VALUES (
+				$1,
+				$2,
+				$3,
+				$4
+			)
+			RETURNING uuid, name, description, target_type_id, account_id, created_at
+		`
+	var _target target
+	if err := pgxscan.Get(ctx, tx, &_target, targetQuery, name, description, targetType.ID, accountID); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	const rtmpQuery = `
+			INSERT INTO target_rtmp (target_id, url, stream_key)
+			VALUES ($1, $2, $3)
+			RETURNING target_id, url, stream_key, created_at
+		`
+	var _rtmp rtmp
+	if err := pgxscan.Get(ctx, tx, &_rtmp, rtmpQuery, _target.ID, url, stream_key); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return &target{}, &rtmp{}, err
+	}
+
+	return &_target, &_rtmp, err
+}
+
+func (repository *repository) delete(ctx context.Context, id string) error {
+	const query = `
+		DELETE FROM target
+		WHERE uuid = $1
+	`
+	cmdTag, err := repository.pool.Exec(ctx, query, id)
+
+	if cmdTag.RowsAffected() == 0 {
+		return sql.ErrNoRows
+	}
+
+	return err
 }
